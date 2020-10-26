c1c0a26c060dd8c82bd6fff9a8b83f5bddbaa922
Benjamin Edwards <benji.edwards60@gmail.com>
2020-10-26 13:53:31 +0000

    v2 prep

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 const chalk = require('chalk');
 module.exports = client => { // eslint-disable-line no-unused-vars
-  console.log(chalk.bgGreen.black('I\'m Online'));
+  console.log('I\'m Online');
 };
