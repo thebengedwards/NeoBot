0a74a96e48daf248f803d59717a51f8d589f2c45
Benjamin Edwards <benji.edwards60@gmail.com>
2020-10-26 15:04:26 +0000

    fixed error

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 const version = require('../package.json').version;
 module.exports = client => {
+  client.user.setActivity('Version: '+version);
   console.log('I\'m Online, Running Version: '+version);
 };
