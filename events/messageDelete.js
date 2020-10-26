19f9621901ff92b109a394fa0ac6a33c8764d7f0
Benjamin Edwards <benji.edwards60@gmail.com>
2020-10-26 16:48:55 +0000

    messagedelete note

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,4 +1,4 @@
-const settings = require('./settings.json');
+const settings = require('../settings.json');
 module.exports = client => {
     client.channels.get(settings.mod).send(`A message with the contents \"${msg.cleanContent}\" was deleted from ${msg.channel}`);
 };
