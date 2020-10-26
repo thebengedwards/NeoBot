414cc3714846bd0594ce6eaa414b5087aedc1b56
Benjamin Edwards <benji.edwards60@gmail.com>
2020-10-26 09:28:28 +0000

    reset

--- neo-bot.js
+++ neo-bot.js
@@ -70,106 +70,106 @@ client.on('messageDelete', msg =>
 
 //Neos Autonomous code goes here
 //Cron uses second, minute, hour, day(of the month, 1-31), Month(of the year, 0-11), and Sunday to Saturday(0-6)
-// let WeeklyFunnyMeme = new cron.CronJob('00 00 20 * * 5', () =>
-// {
-//   let subreddit = "dankmemes";
-//   client.channels.get(memes).send("This week's Funny Meme:");
-//   randomPuppy(subreddit).then(async url => {
-//     await client.channels.get(memes).send({
-//       files:
-//       [{
-//         attachment: url,
-//         name: 'meme.png'
-//       }]
-//     })
-//   }).catch(err => console.error(err));
-// });
-// WeeklyFunnyMeme.start()
+let WeeklyFunnyMeme = new cron.CronJob('00 00 20 * * 5', () =>
+{
+  let subreddit = "dankmemes";
+  client.channels.get(memes).send("This week's Funny Meme:");
+  randomPuppy(subreddit).then(async url => {
+    await client.channels.get(memes).send({
+      files:
+      [{
+        attachment: url,
+        name: 'meme.png'
+      }]
+    })
+  }).catch(err => console.error(err));
+});
+WeeklyFunnyMeme.start()
 
 //Neos Calendar Dates go here
-// let NewYear = new cron.CronJob('00 01 00 01 00 *', () =>
-// {
-//   client.channels.get(general).send("🎉 HAPPY NEW YEAR EVERYONE 🎉");
-// });
-// NewYear.start()
-// let Halloween = new cron.CronJob('00 00 08 31 09 *', () =>
-// {
-//   client.channels.get(general).send("🎃 HAPPY HALLOWEEN EVERYONE 🎃");
-// });
-// Halloween.start()
-// let Christmas = new cron.CronJob('00 00 08 25 11 *', () =>
-// {
-//   client.channels.get(general).send("🎅 MERRY CHRISTMAS EVERYONE 🎅");
-// });
-// Christmas.start()
+let NewYear = new cron.CronJob('00 01 00 01 00 *', () =>
+{
+  client.channels.get(general).send("🎉 HAPPY NEW YEAR EVERYONE 🎉");
+});
+NewYear.start()
+let Halloween = new cron.CronJob('00 00 08 31 09 *', () =>
+{
+  client.channels.get(general).send("🎃 HAPPY HALLOWEEN EVERYONE 🎃");
+});
+Halloween.start()
+let Christmas = new cron.CronJob('00 00 08 25 11 *', () =>
+{
+  client.channels.get(general).send("🎅 MERRY CHRISTMAS EVERYONE 🎅");
+});
+Christmas.start()
 
 
 //Birthdays of Members
-// let BenEBday = new cron.CronJob('00 00 08 14 01 *', () =>
-// {
-//   client.channels.get(general).send("<@271719405384105986>, BEN ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
-// });
-// BenEBday.start()
-// let JamieSBday = new cron.CronJob('00 00 08 10 05 *', () =>
-// {
-//   client.channels.get(general).send("<@271744164377526274>, JAMIE ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
-// });
-// JamieSBday.start()
-// let AlexBBday = new cron.CronJob('00 00 08 03 04 *', () =>
-// {
-//   client.channels.get(general).send("<@271758756595892236>, ALEX ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
-// });
-// AlexBBday.start()
-// let EdBBday = new cron.CronJob('00 00 08 12 06 *', () =>
-// {
-//   client.channels.get(general).send("<@272104323729588225>, ED ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
-// });
-// EdBBday.start()
-// let MaxDBday = new cron.CronJob('00 00 08 23 08 *', () =>
-// {
-//   client.channels.get(general).send("<@385480935900184579>, MAX ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
-// });
-// MaxDBday.start()
-// let OllieMBday = new cron.CronJob('00 00 08 16 03 *', () =>
-// {
-//   client.channels.get(general).send("<@283292935552761857>, OLLIE ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
-// });
-// OllieMBday.start()
-// let AlexHBday = new cron.CronJob('00 00 08 01 03 *', () =>
-// {
-//   client.channels.get(general).send("<@348126167062151180>, ALEX ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
-// });
-// AlexHBday.start()
-// let DylanCBday = new cron.CronJob('00 00 08 09 06 *', () =>
-// {
-//   client.channels.get(general).send("<@272090190657486848>, DYLAN ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
-// });
-// DylanCBday.start()
-// let GenMBday = new cron.CronJob('00 00 08 07 01 *', () =>
-// {
-//   client.channels.get(general).send("<@393047942526926850>, GEN ITS YOUR BIRTHDAY\nCan we all please wish her a happy Birthday!!!");
-// });
-// GenMBday.start()
-// let MilanJBday = new cron.CronJob('00 00 08 07 10 *', () =>
-// {
-//   client.channels.get(general).send("<@277488461844578304>, MILAN ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
-// });
-// MilanJBday.start()
-// let AlexTBday = new cron.CronJob('00 00 08 05 06 *', () =>
-// {
-//   client.channels.get(general).send("<@215951713717190656>, ALEX ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
-// });
-// AlexTBday.start()
-// let FinEBday = new cron.CronJob('00 00 08 28 05 *', () =>
-// {
-//   client.channels.get(general).send("<@286255321264029697>, FIN ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
-// });
-// FinEBday.start()
-// let SamFBday = new cron.CronJob('00 00 08 26 05 *', () =>
-// {
-//   client.channels.get(general).send("<@377971556959256577>, SAM ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
-// });
-// SamFBday.start()
+let BenEBday = new cron.CronJob('00 00 08 14 01 *', () =>
+{
+  client.channels.get(general).send("<@271719405384105986>, BEN ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
+});
+BenEBday.start()
+let JamieSBday = new cron.CronJob('00 00 08 10 05 *', () =>
+{
+  client.channels.get(general).send("<@271744164377526274>, JAMIE ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
+});
+JamieSBday.start()
+let AlexBBday = new cron.CronJob('00 00 08 03 04 *', () =>
+{
+  client.channels.get(general).send("<@271758756595892236>, ALEX ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
+});
+AlexBBday.start()
+let EdBBday = new cron.CronJob('00 00 08 12 06 *', () =>
+{
+  client.channels.get(general).send("<@272104323729588225>, ED ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
+});
+EdBBday.start()
+let MaxDBday = new cron.CronJob('00 00 08 23 08 *', () =>
+{
+  client.channels.get(general).send("<@385480935900184579>, MAX ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
+});
+MaxDBday.start()
+let OllieMBday = new cron.CronJob('00 00 08 16 03 *', () =>
+{
+  client.channels.get(general).send("<@283292935552761857>, OLLIE ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
+});
+OllieMBday.start()
+let AlexHBday = new cron.CronJob('00 00 08 01 03 *', () =>
+{
+  client.channels.get(general).send("<@348126167062151180>, ALEX ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
+});
+AlexHBday.start()
+let DylanCBday = new cron.CronJob('00 00 08 09 06 *', () =>
+{
+  client.channels.get(general).send("<@272090190657486848>, DYLAN ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
+});
+DylanCBday.start()
+let GenMBday = new cron.CronJob('00 00 08 07 01 *', () =>
+{
+  client.channels.get(general).send("<@393047942526926850>, GEN ITS YOUR BIRTHDAY\nCan we all please wish her a happy Birthday!!!");
+});
+GenMBday.start()
+let MilanJBday = new cron.CronJob('00 00 08 07 10 *', () =>
+{
+  client.channels.get(general).send("<@277488461844578304>, MILAN ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
+});
+MilanJBday.start()
+let AlexTBday = new cron.CronJob('00 00 08 05 06 *', () =>
+{
+  client.channels.get(general).send("<@215951713717190656>, ALEX ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
+});
+AlexTBday.start()
+let FinEBday = new cron.CronJob('00 00 08 28 05 *', () =>
+{
+  client.channels.get(general).send("<@286255321264029697>, FIN ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
+});
+FinEBday.start()
+let SamFBday = new cron.CronJob('00 00 08 26 05 *', () =>
+{
+  client.channels.get(general).send("<@377971556959256577>, SAM ITS YOUR BIRTHDAY\nCan we all please wish him a happy Birthday!!!");
+});
+SamFBday.start()
 
 //Neo uses ! to know if it is being told a command
 client.on('message', message =>
