5f64cd6a2c9581a0105e1499509bfcb4acfca15a
Benjamin Edwards <benji.edwards60@gmail.com>
2020-10-26 17:02:36 +0000

    messagedelete note

--- app.js
+++ app.js
@@ -73,10 +73,10 @@ client.on('channelDelete', channel => {
   client.channels.get(general).send(`Channel: ${channel.name} has been deleted`);
 });
 
-client.on('messageDelete', msg =>
-{
-  client.channels.get(admin).send(`A message with the contents \"${msg.cleanContent}\" was deleted from ${msg.channel}`);
-});
+// client.on('messageDelete', msg =>
+// {
+//   client.channels.get(admin).send(`A message with the contents \"${msg.cleanContent}\" was deleted from ${msg.channel}`);
+// });
 
 // //Neos Autonomous code goes here
 // //Cron uses second, minute, hour, day(of the month, 1-31), Month(of the year, 0-11), and Sunday to Saturday(0-6)
