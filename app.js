d00afde53934b7a78c0cc99ccec650826afee926
Ben Edwards <benji.edwards60@gmail.com>
2020-10-27 21:48:07 +0000

    removed comments of legacy code

--- app.js
+++ app.js
@@ -60,23 +60,6 @@ client.elevation = message => {
   return permlvl;
 };
 
-// //Neo's Client Events are here
-// client.on('channelCreate', channel =>
-// {
-//   client.channels.get(admin).send(`A ${channel.type} channel by the name of ${channel.name} was created ${channel.createdAt} with the ID of ${channel.id}`);
-//   if (channel.type === 'text') return channel.send(`This ${channel.type} channel was created ${channel.createdAt} and is called ${channel.name}.`);
-// });
-
-// client.on('channelDelete', channel => {
-//   client.channels.get(admin).send(`A ${channel.type} channel by the name of ${channel.name} was successfully deleted.`);
-//   client.channels.get(general).send(`Channel: ${channel.name} has been deleted`);
-// });
-
-// client.on('messageDelete', msg =>
-// {
-//   client.channels.get(admin).send(`A message with the contents \"${msg.cleanContent}\" was deleted from ${msg.channel}`);
-// });
-
 // //Neos Autonomous code goes here
 // //Cron uses second, minute, hour, day(of the month, 1-31), Month(of the year, 0-11), and Sunday to Saturday(0-6)
 // // let WeeklyFunnyMeme = new cron.CronJob('00 00 20 * * 5', () =>
