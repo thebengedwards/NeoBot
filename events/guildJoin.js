515013965a7d8befd94c2573913314787e9efb3f
Ben Edwards <benji.edwards60@gmail.com>
2020-11-19 14:43:27 +0000

    removed faulty code

--- events/guildJoin.js
+++ events/guildJoin.js
@@ -21,30 +21,4 @@ module.exports = guild => {
      },
   })
     .then(res => res.json())
-
-  const alertEmbed = require('../embeds/alertEmbed');
-  const embed = new Discord.MessageEmbed(alertEmbed);
-
-  embed.setDescription('Config Required');
-  embed.addFields(
-    { name: `NEO has joined: ${guild.name}`, value: 'A config is required' },
-    { name: 'Use \'!config\' in your server to start setting up NEO for your server.', value: 'You can see active settings by using \'!config\'.' },
-    { name: '\u200B', value: '\u200B' },
-    { name: 'Only a server owner or a member of a role called \'Admin\' can start a config.', value: 'This is for security reasons' }
-  )
-
-  let channelID;
-  let channels = guild.channels.cache;
-
-  channelLoop:
-  for (let key in channels) {
-    let c = channels[key];
-    if (c[1].type === "text") {
-      channelID = c[0];
-      break channelLoop;
-    }
-  }
-
-  let channel = guild.channels.cache.get(guild.systemChannelID || channelID);
-  channel.send({ embed });
 };
