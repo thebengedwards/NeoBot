3b5483e43e10f81b256129776c50ced3c13d5bea
Ben Edwards <benji.edwards60@gmail.com>
2020-11-20 14:03:07 +0000

    started adding functions

--- commands/config.js
+++ commands/config.js
@@ -52,7 +52,7 @@ exports.run = async (client, message) => {
       { name: 'Welcome to the NEO setup', value: 'This message will guide you through the setup process' },
       { name: '\u200B', value: '\u200B' },
       { name: 'NEO uses three main roles, Admin, Moderator and Member', value: 'It doesn\'t matter if you have more roles, just assign the roles to any applicable.' },
-      { name: 'Assign these roles by using the command command \'!setupRoles <AdminID> <ModeratorID> <MemberID>\'', value: 'Find the role ID\'s by enabling Dev mode in the settings, and then right clicking the role in server settings, and select \'Copy ID\'. If you wish to leave a role empty, use 0.' },
+      { name: 'Assign these roles by using the command command \'!setAdminID <AdminID>\', \'!setModID <ModID>\', \'!setMemberID <MemberID>\'', value: 'Find the role ID\'s by enabling Dev mode in the settings, and then right clicking the role in server settings, and select \'Copy ID\'. If you wish to leave a role empty, use 0.' },
       { name: '\u200B', value: '\u200B' },
       { name: 'NEO can also uses a default of 6 text channels', value: 'It doesn\'t matter if you don\'t have 6 text channels, just assign these to any applicable.' },
       { name: 'Assign these channels by using the command \'!setupChannels <Welcome> <ModeratorOnly> <General> <Memes> <Games> <Updates>\'', value: 'Find the channel ID\'s by enabling Dev mode in the settings, and then right clicking the channel in the server, and select \'Copy ID\'. If you dont have a channel applicable, use 0.' },
