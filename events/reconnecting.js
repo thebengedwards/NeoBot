2b414baf026f50066f2cfe55484b641b5933a896
Ben Edwards <benji.edwards60@gmail.com>
2020-12-13 22:31:45 +0000

    changed readme file to new professional format ready for task

--- events/reconnecting.js
+++ events/reconnecting.js
@@ -1,3 +1,5 @@
+const moment = require('moment')
+
 module.exports = client => {
-  console.log(`Reconnecting at ${new Date()}`);
+  console.log(`Reconnecting at ${moment(new Date()).format('Do MMMM YYYY HH:mm')}. Last Restart: DATA HERE ago`);
 };
