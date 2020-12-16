497405c262ca54f19db95897d3e2c292271df319
Ben Edwards <benji.edwards60@gmail.com>
2020-12-16 12:06:47 +0000

    changed reconnecting text

--- events/reconnecting.js
+++ events/reconnecting.js
@@ -1,5 +1,5 @@
 const moment = require("moment")
 
 module.exports = client => {
-  console.log(`Reconnecting at ${moment(new Date()).format('Do MMMM YYYY')} at ${moment(new Date()).format('HH:mm:ss')}`);
+  console.log(`Reconnecting on ${moment(new Date()).format('Do MMMM YYYY')} at ${moment(new Date()).format('HH:mm:ss')}`);
 };
